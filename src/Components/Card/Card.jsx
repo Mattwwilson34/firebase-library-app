edfe35336606513c9b2b9f3c681b615dc02386cf
Matt Wilson <mattwwilson34@gmail.com>
2022-03-04 18:47:28 -0500

    Included rest of Book data on Card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -11,12 +11,16 @@ const Card = (props) => {
     published,
     website,
   } = props.book;
-  console.log(props);
   return (
     <div className='Card'>
       <h2 className='Card_Title'>{title}</h2>
-      <h3 className='Card_Author'>{author}</h3>
+      <h3 className='Card_Subtitle'>{subtitle}</h3>
+      <h4 className='Card_Author'>{author}</h4>
       <p className='Card_Description'>{description}</p>
+      <p className='Card_Pages'>{pages}</p>
+      <p className='Card_ISBN'>{isbn}</p>
+      <p className='Card_published'>{published}</p>
+      <p className='Card_Website'>{website}</p>
     </div>
   );
 };
