88a96a4f00c94df4dc5df737462aec05253f2a5f
Matt Wilson <mattwwilson34@gmail.com>
2022-03-04 16:46:12 -0500

    Added Card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import firebaseApp from './firebase';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import Card from './Components/Card/Card';
+// import firebaseApp from './firebase';
 
-const db = getFirestore(firebaseApp);
+// import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-const addData = async () => {
-  try {
-    const docRef = await addDoc(collection(db, 'users'), {
-      first: 'Ada',
-      last: 'Lovelace',
-      born: 1815,
-    });
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-};
-addData();
+// const db = getFirestore(firebaseApp);
+
+// const addData = async () => {
+//   try {
+//     const docRef = await addDoc(collection(db, 'users'), {
+//       first: 'Ada',
+//       last: 'Lovelace',
+//       born: 1815,
+//     });
+//     console.log('Document written with ID: ', docRef.id);
+//   } catch (e) {
+//     console.error('Error adding document: ', e);
+//   }
+// };
 
 function App() {
-  return <div className='App'>Firebase Library</div>;
+  return (
+    <div className='App'>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
