127ab4f0132632deef4ab9be4ceb95375fd590c3
Matt Wilson <mattwwilson34@gmail.com>
2022-03-03 22:02:06 -0500

    added firebase firestore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import './App.css';
+import firebaseApp from './firebase';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+
+const db = getFirestore(firebaseApp);
+const docRef = doc(db, 'test', 'test1');
+const getData = async () => {
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log('Document data:', docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log('No such document!');
+  }
+};
+getData();
 
 function App() {
   return <div className='App'>Firebase Library</div>;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAo5pT2pVlbLAZDUQ3qc26oHdOYPQoA49I',
+  authDomain: 'libraryapp-2bf69.firebaseapp.com',
+  projectId: 'libraryapp-2bf69',
+  storageBucket: 'libraryapp-2bf69.appspot.com',
+  messagingSenderId: '223405078179',
+  appId: '1:223405078179:web:1b93e87021dab7c513b116',
+  measurementId: 'G-Q3QPQGSBRE',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
