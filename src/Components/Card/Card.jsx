46547ceaf2d47e8b48b8755b646005936a758481
Matt Wilson <mattwwilson34@gmail.com>
2022-03-04 17:00:55 -0500

    Added filler text to Card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,21 @@
 import '../../App.css';
 
 const Card = (props) => {
-  return <div className='Card'>Card</div>;
+  return (
+    <div className='Card'>
+      <h2 className='Card_Title'>Harry Potter and the Order of the Phoenix</h2>
+      <h3 className='Card_Author'>By: J.K. Rollings</h3>
+      <p className='Card_Description'>
+        Suspendisse lectus rhoncus vitae mattis aliquam fames interdum aliquet
+        adipiscing tristique tincidunt, fermentum posuere massa purus finibus
+        sit curae ad lorem malesuada. Lacus viverra dapibus ad lectus rhoncus
+        nec mattis facilisi nullam quisque proin integer, nostra nascetur auctor
+        etiam augue scelerisque massa morbi habitant euismod. Condimentum
+        maximus penatibus lacus leo cubilia ac laoreet nisi sodales, habitasse
+        eu nulla mauris erat litora rutrum porttitor.
+      </p>
+    </div>
+  );
 };
 
 export default Card;
