817c5a57697bf0d39224351ebe56c4e5a668a08b
Matt Wilson <mattwwilson34@gmail.com>
2022-03-05 13:34:15 -0500

    Fixed card styling

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import '../../App.css';
 
 const Button = (props) => {
-  const { text, handleClick, read } = props;
+  const { text, handleClick, read, className } = props;
   return (
     <button
-      style={read ? { backgroundColor: 'green' } : {}}
+      className={className}
+      style={read ? { backgroundColor: '#9ae3a3' } : {}}
       onClick={handleClick}>
       {text}
     </button>
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import '../../App.css';
 import Button from '../Button/Button';
+import React, { useState } from 'react';
 
 const Card = (props) => {
   const {
@@ -14,12 +15,24 @@ const Card = (props) => {
     read,
   } = props.book;
   const { handleReadButtonClick } = props;
+
+  const [alreadyRead, setAlreadyRead] = useState(read);
+
   return (
     <div className='Card'>
       <h2 className='Card_Title'>{title}</h2>
       <h3 className='Card_Subtitle'>{subtitle}</h3>
       <h4 className='Card_Author'>By: {author}</h4>
-      <Button text='Read' read={read} handleClick={handleReadButtonClick} />
+      <Button
+        className='Button_Read'
+        text='Read'
+        read={alreadyRead}
+        handleClick={(e) => {
+          handleReadButtonClick(e);
+          setAlreadyRead(!alreadyRead);
+        }}
+      />
+      <Button className='Button_Add_Info' text='Additional Info' />
       <div className='Card_Add_Info_Container'>
         <p className='Card_Description'>{description}</p>
         <p className='Card_Pages'>{pages}</p>
